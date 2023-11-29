c48134c6dfc88c0b8e1630f17404f3dc4283d34f
ahmisrafil <ahmisrafil@yahoo.com>
2023-11-29 11:16:55 +0600

    mentorshub project created

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,13 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Home from "../pages/Home/Home/Home";
+
+
+ export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+  
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h3 className="text-4xl">Welcome Home</h3>
+        </div>
+    );
+};
+
+export default Home;
