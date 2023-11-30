9477ba392c4e17489172e29eb00cb43f738683f8
ahmisrafil <ahmisrafil@yahoo.com>
2023-11-30 14:00:45 +0600

    routed updated

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -2,12 +2,21 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import Home from "../pages/Home/Home/Home";
+import Root from "../components/Root/Root";
+import Error from "../pages/Error/Error";
 
 
  export const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <Root></Root>,
+      errorElement: <Error></Error>,
+      children:[
+        {
+          path: '/',
+          element:<Home></Home>
+        }
+      ]
     },
   ]);
   
---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,11 @@
+
+
+const Error = () => {
+    return (
+        <div>
+            <h3>An error occurred</h3>
+        </div>
+    );
+};
+
+export default Error;
